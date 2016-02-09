16e05566c94faee3fba84b7e49928c54b76d2065
Tom Anderson <eltomo4@gmail.com>
2016-02-08 20:19:46 -0600

    Convert CRUD requests to use current user todos only

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ app.get('/', function(req, res){
 app.get('/todos', middleware.requireAuthentication, function(req, res){
 	var query = req.query;
 
-	var where = {};
+	var where = {
+		userId: req.user.get('id')
+	};
 
 	if(query.hasOwnProperty('completed') && query.completed === 'true'){
 		where.completed = true;
@@ -46,7 +48,12 @@ app.get('/todos', middleware.requireAuthentication, function(req, res){
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res){
 	var todoId = parseInt(req.params.id, 10);
 
-	db.todo.findById(todoId).then(function(todo){
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo){
 		if(!!todo){ //runs if there IS a todo item
 			res.json(todo.toJSON());
 		}else{
@@ -63,7 +70,6 @@ app.post('/todos', middleware.requireAuthentication, function(req, res){
 	var body = _.pick(req.body, 'description', 'completed');
 
 	db.todo.create(body).then(function(todo){
-		//res.json(todo.toJSON());
 		req.user.addTodo(todo).then(function (){
 			return todo.reload();
 		}).then(function(todo){
@@ -81,7 +87,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res){
 
 	db.todo.destroy({
 		where: {
-			id: todoId
+			id: todoId,
+			userId: req.user.get('id')
 		}
 	}).then(function(rowsDeleted){
 		if(rowsDeleted === 0){
@@ -111,7 +118,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res){
 		attributes.description = body.description;
 	}
 
-	db.todo.findById(todoId).then(function(todo){
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo){
 		if(todo){
 			todo.update(attributes).then(function(todo){
 				res.json(todo.toJSON());
